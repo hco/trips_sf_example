c87b188170b8ef443e39c1806506e0ec47354c69
Hans-Christian Otto <c.otto@suora.com>
2018-05-01 15:38:01 +0200

    Added request classes

---
+++ src/Request/AddTripRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Request;
+
+class AddTripRequest extends Request
+{
+    public $name;
+    public $start;
+    public $end;
+}
---
+++ src/Request/Request.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Request;
+
+class Request
+{
+
+}
